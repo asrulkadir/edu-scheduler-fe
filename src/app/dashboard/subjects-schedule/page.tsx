8ca905f31a768a46c193c800742b402c0f5c36af
Asrul Kadir <asrulkadir.asus4@gmail.com>
2025-01-03 16:47:45 +0700

    initialize subject shcedule page

--- src/app/dashboard/subjects-schedule/page.tsx
+++ src/app/dashboard/subjects-schedule/page.tsx
@@ -1,7 +1,27 @@
 'use client';
 
+import { Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
 const Page = () => {
-  return <div>Halaman Jadwal Mata Pelajaran</div>;
+  return (
+    <>
+      <div>
+        <h1 className="mb-6 text-2xl font-extrabold text-gray-800">
+          Daftar Mata Pelajaran
+        </h1>
+        <Button
+          className="mb-6"
+          icon={<PlusOutlined />}
+          // onClick={() => showModalAddEdit('')}
+          type="primary"
+          size="large"
+        >
+          Tambah Jadwal Mata Pelajaran
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default Page;
